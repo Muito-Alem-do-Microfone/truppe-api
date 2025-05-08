c90e47cdd7beade925d8eb36d99b3cbf357c794c
Alisson Reis <alissonoreis@gmail.com>
2025-05-08 15:05:50 -0300

    MADM-114 Adds states test

--- src/routes/states.routes.js
+++ src/routes/states.routes.js
@@ -1,10 +1,10 @@
-import express from express;
-import {getStates} from "../controllers/states.controller.js";
+import express from "express";
+import { getStates } from "../controllers/states.controller.js";
 
 const router = express.Router();
 
 export default (app) => {
-    router.get("/", getStates);
+  router.get("/", getStates);
 
-    app.use("api/states", router); 
+  app.use("/api/states", router);
 };
